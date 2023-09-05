e5f2ab9098fe193b15a9dc7ef262e11aba710a95
Rabbok <rabbokk@gmail.com>
2023-09-05 13:15:20 +0300

    fix: fixed slider

--- components/partials/ScrollableSkills.tsx
+++ components/partials/ScrollableSkills.tsx
@@ -1,23 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Slider from 'react-slick';
 import SkillElement from './SkillElement';
 import styles from './partials.module.scss';
-
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import { useMediaQuery } from 'react-responsive';
 
 const ScrollableSkills: React.FC = () => {
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+  const [windowWidth, setWindowWidth] = useState<number | undefined>(undefined);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setWindowWidth(window.innerWidth);
+      const updateWindowWidth = () => {
+        setWindowWidth(window.innerWidth);
+      };
+
+      window.addEventListener('resize', updateWindowWidth);
+
+      return () => {
+        window.removeEventListener('resize', updateWindowWidth);
+      };
+    }
+  }, []);
+
   const settings = {
     dots: false,
     infinite: true,
     speed: 500,
-    slidesToShow: 6,
-    slidesToScroll: 1,
     arrows: false,
+    slidesToShow: isMobile ? 4 : 6,
+    slidesToScroll: 1,
   };
-  
+
   return (
-    <div className={styles["skills-container"]}>
+    <div className={styles['skills-container']}>
       <Slider {...settings}>
         <SkillElement name="HTML" src="img/html-icon.png" />
         <SkillElement name="CSS" src="img/css-icon.png" />
